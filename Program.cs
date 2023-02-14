4a51c08ac993376bca09729be54d12c5fdae4113
Alessandra Soares dos Santos <alessandra.ss@edu.sc.senai.br>
2023-02-14 19:30:32 -0300

    inclusao

--- Program.cs
+++ Program.cs
@@ -12,7 +12,6 @@ using System.Text;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
-
 builder.Services.AddEndpointsApiExplorer();
  
 builder.Services.AddControllersWithViews().AddNewtonsoftJson();
